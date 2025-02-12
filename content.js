2817f0a364177f9267f24f27bf4cf2872c351e3d
Felix Aldam-Gates <aldam.gatesfelix@gmail.com>
2025-02-12 16:26:28 +0000

    Moved stop conditions logic into seperate function. Adjusted so that URLs that don't include any flags also are redirected to use the correct authuser=

--- content.js
+++ content.js
@@ -4,30 +4,54 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     }
 });
 
+function checkStopConditionsMet(enabled, urlBeforeParameters, selectedAccountNumber) {
+    const stopConditionMet = false;
+
+    // Stop if disabled
+    if (!enabled) { stopConditionMet = true; }
+
+    // Stop if not on a Google Cloud URL (h)
+    const isCorrectURL = urlBeforeParameters.includes('google') && urlBeforeParameters.includes('cloud');
+    if (!isCorrectURL) { stopConditionMet = true; }
+ 
+    // Check if an account number has been set
+    if (!selectedAccountNumber) { stopConditionMet = true; }
+
+    // If the current url includes "authuser={selectedAccountNumber}" then stop
+    if (window.location.href.includes(`authuser=${selectedAccountNumber}`)) { stopConditionMet = true; }
+
+}
+
 chrome.storage.local.get(['accountNumber', 'enabledStatus'], function(data) {
     const currentURL = window.location.href;
     const selectedAccountNumber = data.accountNumber;
     const enabled = data.enabledStatus;
 
     // Split the URL to get the part before query parameters
-    const urlBeforeParameters = currentURL.split('?')[0];
+    const urlBeforeParameters = currentURL.split('?')[0]; // console.cloud.google.com/xyz
 
-    // Check if the URL contains 'google' and 'cloud' before the query parameters
-    const isCorrectURL = urlBeforeParameters.includes('google') && urlBeforeParameters.includes('cloud');
+    // Check if the stop conditions are met and stop here if they are
+    const stopConditionMet = checkStopConditionsMet(enabled, urlBeforeParameters, selectedAccountNumber);
+    if (stopConditionMet) { return; } else {
+        console.log('Stop conditions not met, continuing...');
 
-    // Only if toggled on and the URL contains 'google' and 'cloud'
-    if (enabled && isCorrectURL) {
-      // Check if an account number has been set
-      if (!selectedAccountNumber) {
-          console.log('No account number set. Doing nothing.');
-          return; // Exit the function if no account number is set
-      }
-
-      if (currentURL.includes("authuser=") && !currentURL.includes(`authuser=${selectedAccountNumber}`)) {
-          // Replace "authuser=XX" with the selected account number
-          const urlWithUpdatedUser = currentURL.replace(/authuser=\d+/, `authuser=${selectedAccountNumber}`);
-          window.location.href = urlWithUpdatedUser;
-      }
+        // If no parameters, add a new one specifying current user
+        const hasParameters = currentURL.includes('?');
+        if (!hasParameters) {
+            const newURL = `${currentURL}?authuser=${selectedAccountNumber}`;
+            window.location.href = newURL;
+        } else {
+            // Parameters exist, but might not include the authuser parameter.
+            if (currentURL.includes('authuser')) {
+                // Authuser is param, change it
+                const urlWithUpdatedUser = currentURL.replace(/authuser=\d+/, `authuser=${selectedAccountNumber}`);
+                window.location.href = urlWithUpdatedUser;
+            } else {
+                // Authuser is not a param, but other params exist, so add it
+                const urlWithNewParameter = `${currentURL}&authuser=${selectedAccountNumber}`;
+                window.location.href = urlWithNewParameter;
+            }
+        }
     }
 });
 
